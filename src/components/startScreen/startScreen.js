af4ac87e625581d79f209b2d1a3e1acabb1b6ee2
zadnap <zadnap@users.noreply.github.com>
2025-01-22 22:02:38 +0700

    fix(credit): link to TOP is now opened in new page

--- src/components/startScreen/startScreen.js
+++ src/components/startScreen/startScreen.js
@@ -19,7 +19,7 @@ function createStartScreen() {
   const credit = document.createElement('p');
   credit.className = 'credit';
   credit.innerHTML =
-    'Part of <a href="https://www.theodinproject.com/">TOP curriculum</a>';
+    'Part of <a href="https://www.theodinproject.com/dashboard" target="_blank" >TOP curriculum</a>';
   startScreen.appendChild(credit);
 
   const loading = createLoading();
