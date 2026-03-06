c0fd03189a4af4b1da0f45eb18b1720be83a88be
Zadnap <88957683+zadnap@users.noreply.github.com>
2026-03-06 08:53:25 +0700

    fix(styles): remove the scroll from main screen
    I tried to fit everything in one screen to remove the scrollbar. This
    makes the game experience better for player since they do not have to
    scroll up and down to see the shipyard and the board.

--- src/components/shipyard/shipyard.js
+++ src/components/shipyard/shipyard.js
@@ -5,11 +5,6 @@ function createShipyard(ships) {
   const shipyard = document.createElement('div');
   shipyard.className = 'shipyard';
 
-  const title = document.createElement('h3');
-  title.className = 'title';
-  title.textContent = 'shipyard';
-  shipyard.appendChild(title);
-
   const yardList = document.createElement('div');
   yardList.className = 'yard-list';
   ships.forEach((ship) => {
